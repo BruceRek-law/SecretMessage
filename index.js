66eebd94aaa54a8368d14cb2ee71f4a73e691921
BruceRek-law <brucewalker.dev@gmail.com>
2022-05-31 16:41:18 -0500

    Phase#1

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+const {hash} = window.location;
+
+const message =atob(hash.substring(1));
+
+if(message){
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#message-show').classList.remove('hide');
+
+    document.querySelector('h1').innerHTML = message;
+}
+
 document.querySelector('form').addEventListener('submit', event =>{
     event.preventDefault();
+
+    document.querySelector('#message-form').classList.add('hide')
     console.log("form submitted")
+    document.querySelector('#link-form').classList.remove('hide')
     const input = document.querySelector('#message-input')
     const encrypted = btoa(input.value);
-    document.querySelector('#link-input').value = encrypted;
-    console.log(`${encrypted}`);
+    const URL = `${window.location}#${encrypted}`;
+    document.querySelector('#link-input').value = URL;
+    console.log(`${window.location+encrypted}`);
 })
